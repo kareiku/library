0e0927c932a7780ba04d07d1a0f71ea9291e3690
kareiku <xaxvu7@gmail.com>
2024-12-27 18:09:41 +0100

    empty commit message

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const filePath = './data.json';
-const fs = require('fs');
-
-let data = fs.readFileSync(filePath, 'utf8');
-data = JSON.parse(data);
-
-data.forEach(o => console.log(o));
+fetch('./data.json')
+    .then(response => response.json())
+    .then(data => {
+        let table = document.getElementById('results');
+        data.forEach(item => {
+            let values = [item.name, item.author, item.genre, item.category, item.subcategory, item.src];
+            let row = table.insertRow();
+            values.forEach(value => {
+                let cell = row.insertCell();
+                cell.innerHTML = value || null;
+            });
+        });
+    })
+    .catch(error => console.error(error));
