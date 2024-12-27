dcdd3fc6fb0688f37cd4ef70ecf9bfbf71554ac8
kareiku <xaxvu7@gmail.com>
2024-12-27 18:27:32 +0100

    empty commit message

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-fetch('./data.json')
+fetch("./data.json")
     .then(response => response.json())
     .then(data => {
-        let table = document.getElementById('results');
+        let table = document.getElementById("results");
         data.forEach(item => {
             let values = [item.name, item.author, item.genre, item.category, item.subcategory, item.src];
             let row = table.insertRow();
             values.forEach(value => {
                 let cell = row.insertCell();
                 cell.innerHTML = value || null;
+                if (const match = cell.innerHTML.match(/https?:\/\/[^\s]+/)) {
+                    const img = document.createElement("img");
+                    img.src = match[0];
+                    cell.innerHTML = "";
+                    cell.appendChild(img);
             });
         });
     })
     .catch(error => console.error(error));
+
+const tableData = document.getElementById("results");
+console.log(typeof tableData);
