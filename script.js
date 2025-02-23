5bcdb4c631efe12b38a46cf904fb0c36c8870acb
kareiku <xaxvu7@gmail.com>
2025-02-24 00:44:23 +0100

    Data is now automatically searched by fetch. Events are used instead of old html attributes. The algorithms have been improvedi n general.

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
-let originalData = null;
-fetch("./data.json").then(response => response.json()).then(data => filterTable(originalData = data)).catch(error => console.error(error));
+let entries = null;
 
-function populateTable(data) {
-    const table = document.getElementById("results");
-    const header = table.rows[0];
+const table = document.getElementById("results");
+const field = document.getElementById("selector");
+const input = document.getElementById("search-bar");
+
+window.addEventListener("load", () => {
+    fetch("/data")
+        .then(response => response.json())
+        .then(data => entries = data)
+        .then(() => loadTable())
+        .then(() => input.focus())
+        .catch(error => console.error(error));
+});
+
+field.addEventListener("mousedown", () => input.focus());
+input.addEventListener("keyup", () => loadTable());
+
+function loadTable() {
+    const filteredData = input !== "" ? entries.filter(entry => (entry[field.value] || "").toLowerCase().includes(input.value.toLowerCase())) : entries;
+    const keys = entries && entries.length > 0 ? Object.keys(entries[0]) : [];
     table.innerHTML = "";
-    table.appendChild(header);
-    data.forEach(record => {
-        const row = table.insertRow();
-        record = [record.name, record.author, record.category, record.genre, record.progress];
-        record.forEach(field => {
-            const cell = row.insertCell();
-            cell.innerHTML = field === false ? false : field || "";
-        });
-    });
-}
 
-function filterTable() {
-    const column = document.getElementById("selector").value;
-    const query = document.getElementById("filter").value;
-    populateTable(query ? originalData.filter(record => (record[column] || "").toLowerCase().includes(query.toLowerCase())) : originalData);
+    const header = document.createElement("tr");
+    keys.forEach(key => {
+        const th = document.createElement("th");
+        th.textContent = key.charAt(0).toUpperCase() + key.slice(1);
+        header.appendChild(th);
+    });
+    table.append(header);
+
+    filteredData.forEach(datum => {
+        const row = table.insertRow();
+        keys.forEach(key => row.insertCell().innerHTML = datum[key]);
+    });
 }
